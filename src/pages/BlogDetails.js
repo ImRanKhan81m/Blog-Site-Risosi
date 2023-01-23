97cd6a18227b6b15912bea77ff2b0dc7f2f82f15
Md Imran Hossen <freelancerimran81@gmail.com>
2023-01-23 10:59:36 +0600

    loading set

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { articleDataContext } from '../App';
 import LatestBlogItem from '../component/LatestBlog/LatestBlogItem';
+import Loading from '../component/Shared/Loading';
 
 const BlogDetails = () => {
     const [blog, setBlog] = useState({})
@@ -25,43 +26,49 @@ const BlogDetails = () => {
     const relatedBlogs = blogs?.filter((blog) => blog?._id !== id);
 
     return (
-        <div className='mid-container'>
-            <div className='pt-10 md:pb-20 pb-5'>
-               <div className='w-full md:h-96 sm:h-60 h-36'>
-               <img className='w-full h-full object-cover mb-3' src={blog?.image} alt="" />
-               </div>
-                <h2 className='mt-5 font-bold text-2xl mb-2'>{blog?.title}</h2>
-                {
-                    blog?.tags?.map((tag) => (
-                        <span className=' text-xs text-white bg-secondary px-3 py-1 rounded-full mr-2'>{tag}</span>
-                    ))
-                }
+        <>
+            {
+                loading ? <Loading /> : (
+                    <div className='mid-container'>
+                        <div className='pt-10 md:pb-20 pb-5'>
+                            <div className='w-full md:h-96 sm:h-60 h-36'>
+                                <img className='w-full h-full object-cover mb-3' src={blog?.image} alt="" />
+                            </div>
+                            <h2 className='mt-5 font-bold text-2xl mb-2'>{blog?.title}</h2>
+                            {
+                                blog?.tags?.map((tag) => (
+                                    <span className=' text-xs text-white bg-secondary px-3 py-1 rounded-full mr-2'>{tag}</span>
+                                ))
+                            }
 
-                <div className=' flex justify-between mb-3 mt-3'>
-                    <p className='font-bold'>{blog?.authorName}</p>
-                    <p>{blog?.publishDate}</p>
-                </div>
+                            <div className=' flex justify-between mb-3 mt-3'>
+                                <p className='font-bold'>{blog?.authorName}</p>
+                                <p>{blog?.publishDate}</p>
+                            </div>
 
-                <div
-                    className='text-xs pb-3 pt-1'
-                    dangerouslySetInnerHTML={{ __html: blog?.description }}
-                >
-                </div>
+                            <div
+                                className='text-xs pb-3 pt-1'
+                                dangerouslySetInnerHTML={{ __html: blog?.description }}
+                            >
+                            </div>
 
-            </div>
+                        </div>
 
-            <h1 className='font-semibold text-2xl border-b-[1px] w-52 pb-1 mb-5'>Related Blogs</h1>
-            <div className='grid md:grid-cols-3 sm:grid-cols-2 gap-5 pb-20'>
-                {
-                    relatedBlogs?.slice(0, 3).map((blog) => (
-                        <LatestBlogItem
-                            key={blog?._id}
-                            blog={blog}
-                        />
-                    ))
-                }
-            </div>
-        </div>
+                        <h1 className='font-semibold text-2xl border-b-[1px] w-52 pb-1 mb-5'>Related Blogs</h1>
+                        <div className='grid md:grid-cols-3 sm:grid-cols-2 gap-5 pb-20'>
+                            {
+                                relatedBlogs?.slice(0, 3).map((blog) => (
+                                    <LatestBlogItem
+                                        key={blog?._id}
+                                        blog={blog}
+                                    />
+                                ))
+                            }
+                        </div>
+                    </div>
+                )
+            }
+        </>
     );
 };
 
